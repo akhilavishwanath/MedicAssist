a8ddd85d5307501525b6f6db83b4f6a6781cda15
Akhila Vishwanath <akhila.vishwanath56@gmail.com>
2026-06-29 15:57:10 +0530

    added recorder

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import {
+    startRecording,
+    stopRecording
+} from "./components/recorder.js";
 import { showToast } from "./components/utils.js";
 import {
     updateSpeechStatus,
@@ -20,10 +24,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const generateBtn = document.getElementById("generateBtn");
     const medicalText = document.getElementById("medicalText");
-
+    const startBtn = document.getElementById("startRecording");
+    const stopBtn = document.getElementById("stopRecording");
+    let recordedAudio = null;
     resetStatus();
     clearTranscript();
     clearFHIR();
+    startBtn.addEventListener("click", async () => {
+
+    const started = await startRecording();
+
+    if (started) {
+
+        updateSpeechStatus("Recording...");
+
+        showToast("Recording Started");
+
+    }
+
+});
+
+stopBtn.addEventListener("click", async () => {
+    const result = await stopRecording();
+
+recordedAudio = result.blob;
+
+updateSpeechStatus("Recording Completed");
+
+showToast(`Recording Saved (${result.duration} seconds)`);
+
+console.log(result);
+});
 
     generateBtn.addEventListener("click", () => {
 
---
+++ src/components/recorder.js
@@ -0,0 +1,94 @@
+let mediaRecorder;
+let audioChunks = [];
+
+let timer = null;
+let seconds = 0;
+
+export async function startRecording() {
+
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true
+        });
+
+        mediaRecorder = new MediaRecorder(stream);
+
+        audioChunks = [];
+
+        seconds = 0;
+
+        updateTimer();
+
+        timer = setInterval(() => {
+
+            seconds++;
+
+            updateTimer();
+
+        }, 1000);
+
+        mediaRecorder.ondataavailable = (event) => {
+            audioChunks.push(event.data);
+        };
+
+        mediaRecorder.start();
+
+        return true;
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Microphone permission denied.");
+
+        return false;
+    }
+
+}
+
+export function stopRecording() {
+
+    return new Promise((resolve) => {
+
+        if (!mediaRecorder) {
+
+            resolve(null);
+
+            return;
+
+        }
+
+        clearInterval(timer);
+
+        mediaRecorder.onstop = () => {
+
+            const audioBlob = new Blob(audioChunks, {
+
+                type: "audio/webm"
+
+            });
+
+            resolve({
+                blob: audioBlob,
+                duration: seconds
+            });
+
+        };
+
+        mediaRecorder.stop();
+
+    });
+
+}
+
+function updateTimer() {
+
+    const minutes = String(Math.floor(seconds / 60)).padStart(2, "0");
+
+    const secs = String(seconds % 60).padStart(2, "0");
+
+    document.getElementById("recordingTimer").textContent =
+        `${minutes}:${secs}`;
+
+}
